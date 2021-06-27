3ed5a4f0851e05715fb45417fe567a4e9bdd9171
gigiperih <gilangabdulaziz@live.com>
2021-06-27 15:48:04 +0700

    init parsing test

--- app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
+++ app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
@@ -1,4 +1,15 @@
 package io.gigiperih.cityx
 
+import org.junit.Test
+
 class CityRepositoryTest {
+    @Test
+    fun `given valid json file, when parsing is succeed, should return correct CityJsonResponse`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `given valid json file, when parsing is failing, should return null`() {
+        TODO("Not yet implemented")
+    }
 }

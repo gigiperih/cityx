841a32d731042eb2aa42234e03a92ccfd0178e9b
gigiperih <gilangabdulaziz@live.com>
2021-06-29 23:21:35 +0700

    prepare mapper test

--- app/src/test/java/io/gigiperih/cityx/DomainTest.kt
+++ app/src/test/java/io/gigiperih/cityx/DomainTest.kt
@@ -5,8 +5,10 @@ import kotlin.system.measureTimeMillis
 
 class DomainTest {
     @Test
-    fun `verify search function is consume better than linear time`() {
+    fun `verify search function is executed in a better than linear time-complexity`() {
         // https://stackoverflow.com/questions/44099480/how-can-i-get-the-time-it-takes-a-function-to-test-the-performance-of-functions
+
+        // assuming data are mapped to hashmap we'll get better result than linear-time
         val time = measureTimeMillis {
             doNothing()
         }
---
+++ app/src/test/java/io/gigiperih/cityx/MapperTest.kt
@@ -0,0 +1,10 @@
+package io.gigiperih.cityx
+
+import org.junit.Test
+
+class MapperTest {
+    @Test
+    fun `verify data from repository is mapped to HashMap`() {
+        TODO("Not yet implemented")
+    }
+}

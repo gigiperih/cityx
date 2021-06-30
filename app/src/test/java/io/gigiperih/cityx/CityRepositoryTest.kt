e63636981e6daadf37707483913f757c75dec6de
gigiperih <gilangabdulaziz@live.com>
2021-06-30 22:30:47 +0700

    refactor repo

--- app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
+++ app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
@@ -1,11 +1,8 @@
 package io.gigiperih.cityx
 
 import com.google.common.truth.Truth.assertThat
-import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.JsonEncodingException
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.gigiperih.cityx.arch.BaseCityTest
 import io.gigiperih.cityx.data.City
 import io.gigiperih.cityx.data.repository.CityRepositoryImpl
 import io.gigiperih.cityx.data.source.ResourceService
@@ -18,16 +15,8 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import kotlin.system.measureNanoTime
-import kotlin.system.measureTimeMillis
 
-class CityRepositoryTest {
-    private val singleDataSet = getResource(fileName = "city.json")
-    private val smallDataSet = getResource(fileName = "cities_2.json")
-    private val mediumDataSet = getResource(fileName = "cities_100.json")
-    private val largeDataSet = getResource(fileName = "cities_20k.json")
-    private val incompleteDataSet = getResource(fileName = "incomplete.json")
-    private val invalidDataSet = getResource(fileName = "invalid.json")
-
+class CityRepositoryTest : BaseCityTest() {
     var service: ResourceService = mockk(relaxed = true)
     private lateinit var objectUnderTest: CityRepository
 
@@ -66,73 +55,101 @@ class CityRepositoryTest {
 
     @Test
     fun `given valid large json file, when parsing is succeed, should return correct list of cities`() {
-        val result = loadData(largeDataSet)
+        every { service.get("cities_20k.json") } returns largeDataSet
+
+        val result = objectUnderTest.get("cities_20k.json")
 
         assertThat(result).apply {
             hasSize(20000)
             containsNoDuplicates()
         }
+
+        verify { service.get("cities_20k.json") }
     }
 
     @Test
     fun `given valid but incomplete json file, when parsing is failing, should return null`() {
-        val result = loadData(incompleteDataSet)
+        every { service.get("incomplete.json") } returns incompleteDataSet
+
+        val result = objectUnderTest.get("incomplete.json")
 
         assertThat(result).apply {
             isNull()
         }
+
+        verify { service.get("incomplete.json") }
     }
 
     @Test(expected = JsonEncodingException::class)
     fun `given invalid json file, when parsing is failing, should throws JsonEncodingException`() {
-        loadData(invalidDataSet)
+        every { service.get("invalid.json") } returns invalidDataSet
+
+        objectUnderTest.get("invalid.json")
     }
 
     @Test
     fun `given unsorted small list of city, when sorting is success, should return sorted list`() {
-        val unsortedList = loadData(smallDataSet)
+        every { service.get("cities_2.json") } returns smallDataSet
+
+        val unsortedList = objectUnderTest.get("cities_2.json")
         val result = unsortedList.sortAlphabetically()
 
         assertThat(result).apply {
             isEqualTo(FakeData.sortedSample)
         }
+
+        verify { service.get("cities_2.json") }
     }
 
     @Test
     fun `given unsorted medium list of city, when sorting is success, should return sorted list`() {
-        val unsortedList = loadData(mediumDataSet)
+        every { service.get("cities_100.json") } returns mediumDataSet
+
+        val unsortedList = objectUnderTest.get("cities_100.json")
         val result = unsortedList.sortAlphabetically()
 
         assertThat(result).apply {
             hasSize(100)
         }
+
+        verify { service.get("cities_100.json") }
     }
 
     @Test
     fun `given unsorted large list of city, when sorting is success, should return sorted list`() {
-        val unsortedList = loadData(largeDataSet)
+        every { service.get("cities_20k.json") } returns largeDataSet
+
+        val unsortedList = objectUnderTest.get("cities_20k.json")
         val result = unsortedList.sortAlphabetically()
 
         assertThat(result).apply {
             hasSize(20000)
         }
+
+        verify { service.get("cities_20k.json") }
     }
 
     @Test
     fun `given null value of city, when sorting is failing, should return null`() {
-        val unsortedList = loadData(incompleteDataSet)
+        every { service.get("incomplete.json") } returns incompleteDataSet
+
+        val unsortedList = objectUnderTest.get("incomplete.json")
         val result = unsortedList.sortAlphabetically()
 
         assertThat(result).apply {
             isNull()
         }
+
+        verify { service.get("incomplete.json") }
     }
 
     @Test
     fun `given valid large and small json file, relative sorting time complexity should be better than linear`() {
+        every { service.get("city.json") } returns singleDataSet
+        every { service.get("cities_20k.json") } returns largeDataSet
 
-        val singleData = loadData(singleDataSet)
-        val largeData = loadData(largeDataSet)
+        val singleData = objectUnderTest.get("city.json")
+        val largeData = objectUnderTest.get("cities_20k.json")
 
         val timeExecSmall = measureNanoTime {
             singleData.sortAlphabetically()
@@ -152,26 +169,13 @@ class CityRepositoryTest {
         // if a list contains 1 element took ~1ns to complete
         // then 20k sized list should be ~20000ns
 
-        println("large $timeExecLarge")
-        println("small $timeExecSmall")
         // requirement: should be better than linear time complexity: O(n)
         assertThat(timeExecLarge).apply {
             isLessThan(timeExecSmall * 20000)
         }
-    }
 
-    // TODO refactor
-    private fun loadData(json: String?): List<City>? {
-        return try {
-            val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-            val listType = Types.newParameterizedType(List::class.java, City::class.java)
-
-            val adapter = moshi.adapter<List<City>>(listType)
-
-            json?.let { adapter.fromJson(it) }
-        } catch (e: JsonDataException) {
-            null
-        }
+        verify { service.get("city.json") }
+        verify { service.get("cities_20k.json") }
     }
 
     // TODO refactor (might use faster algorithm?)
@@ -179,7 +183,4 @@ class CityRepositoryTest {
         if (this == null) return null
         return this.sortedBy { it.name }
     }
-
-    private fun getResource(fileName: String) =
-        this::class.java.classLoader?.getResource(fileName)?.readText()
 }

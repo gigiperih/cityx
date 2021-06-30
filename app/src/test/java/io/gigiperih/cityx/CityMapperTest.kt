ae0b69ff56568cec71966846498ac60f1444f2cf
gigiperih <gilangabdulaziz@live.com>
2021-06-30 14:15:46 +0700

    add mapper test toHashMap

--- app/src/test/java/io/gigiperih/cityx/CityMapperTest.kt
+++ app/src/test/java/io/gigiperih/cityx/CityMapperTest.kt
@@ -1,10 +1,21 @@
 package io.gigiperih.cityx
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class CityMapperTest {
     @Test
     fun `given sorted list of city, when mapped toHashMap, should return correct hashMap`() {
+        val result = FakeData.expectedSample.toHashMap()
+
+        assertThat(result).apply {
+            hasSize(2)
+            isEqualTo(FakeData.hashMapOfExpectedSample)
+        }
+    }
+
+    @Test
+    fun `given null list of city, when mapped toHashMap, should return null`() {
         TODO("Not yet implemented")
     }
 
--- app/src/test/java/io/gigiperih/cityx/FakeData.kt
+++ app/src/test/java/io/gigiperih/cityx/FakeData.kt
@@ -41,4 +41,19 @@ object FakeData {
             coord = Coordinate(lon = 37.666668, lat = 55.683334)
         )
     )
+
+    val hashMapOfExpectedSample = hashMapOf(
+        "Hurzuf UA" to City(
+            country = "UA",
+            name = "Hurzuf",
+            _id = 707860,
+            coord = Coordinate(lon = 34.283333, lat = 44.549999)
+        ),
+        "Novinki RU" to City(
+            country = "RU",
+            name = "Novinki",
+            _id = 519188,
+            coord = Coordinate(lon = 37.666668, lat = 55.683334)
+        )
+    )
 }

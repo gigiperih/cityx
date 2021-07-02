af9a1429fbe1d144778dc3012639fd8ac2f2c7f8
gigiperih <gilangabdulaziz@live.com>
2021-07-02 10:25:14 +0700

    clean up

--- app/src/test/java/io/gigiperih/cityx/CityDomainTest.kt
+++ app/src/test/java/io/gigiperih/cityx/CityDomainTest.kt
@@ -5,18 +5,7 @@ import kotlin.system.measureTimeMillis
 
 class CityDomainTest {
     @Test
-    fun `verify search function is executed in a better than linear time-complexity`() {
-        // https://stackoverflow.com/questions/44099480/how-can-i-get-the-time-it-takes-a-function-to-test-the-performance-of-functions
+    fun `trivial`() {
 
-        // assuming data are mapped to hashmap we'll get better result than linear-time
-        val time = measureTimeMillis {
-            doNothing()
-        }
-
-        TODO("Not yet implemented")
-    }
-
-    private fun doNothing() {
-        // do nothing
     }
 }
--- app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
+++ app/src/test/java/io/gigiperih/cityx/CityRepositoryTest.kt
@@ -220,6 +220,11 @@ class CityRepositoryTest : BaseCityTest() {
     // TODO refactor (might use faster algorithm?)
     private fun List<City>?.sortAlphabetically(): List<City>? {
         if (this == null) return null
+
+        // https://docs.oracle.com/javase/10/docs/api/java/util/Arrays.html#sort(byte%5B%5D)
+        // `Collection.sort()` works by calling the underlying `Arrays.sort()` method,
+        // while the sorting itself uses `Insertion Sort` for arrays shorter than 47,
+        // and `Quicksort` for the rest.
         return this.sortedBy { it.name }
     }
 }

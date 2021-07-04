6c8d859b0c0fe5051ced093f35597fd82f1ce409
gigiperih <gilangabdulaziz@live.com>
2021-07-05 01:51:17 +0700

    implement splash :beer:

--- app/src/main/java/io/gigiperih/cityx/presentation/CityActivity.kt
+++ app/src/main/java/io/gigiperih/cityx/presentation/CityActivity.kt
@@ -1,7 +1,15 @@
 package io.gigiperih.cityx.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.gigiperih.cityx.R
 
 
-class CityActivity : AppCompatActivity(R.layout.activity_city)
+class CityActivity : AppCompatActivity(R.layout.activity_city) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.Theme_CityX)
+        super.onCreate(savedInstanceState)
+    }
+}
